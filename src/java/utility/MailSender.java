5361381fbd3fc6cc642d5085b64464ae385b5eec
nikli <nikli@localhost>
2017-09-16 07:45:26 +0400

    SendGrid mail api applied

--- src/java/utility/MailSender.java
+++ src/java/utility/MailSender.java
@@ -5,6 +5,14 @@
  */
 package utility;
 
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.Message;
@@ -16,31 +24,36 @@ import javax.mail.internet.MimeMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import org.apache.logging.log4j.LogManager;
 
 /**
  *
  * @author nikli
  */
 public class MailSender {
+    
+    private static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
 
     public static void send(String toEmail, String subject, String text) {
-        try {
-            Context initCtx = new InitialContext();
-            Context envCtx = (Context) initCtx.lookup("java:comp/env");
-            Session session = (Session) envCtx.lookup("mail/Session");
+        final String sendgridApiKey = System.getenv("SENDGRID_API_KEY");
+        //final String sendgridSender = System.getenv("SENDGRID_SENDER");
+        Email from = new Email("APDVoice <no-reply@apdvoice.com>");
+        Email to = new Email(toEmail);
+        Content content = new Content("text/html", text);
+        Mail mail = new Mail(from, subject, to, content);
 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("no-reply@apdvoice.com"));
-            InternetAddress to[] = new InternetAddress[1];
-            to[0] = new InternetAddress(toEmail);
-            message.setRecipients(Message.RecipientType.TO, to);
-            message.setSubject(subject);
-            message.setContent(text, "text/plain");
-            Transport.send(message);
-        } catch (Exception ex) {
-            System.out.println("Mail couldn't send: "+ex.getMessage());
-            Logger.getLogger(MailSender.class.getName()).log(Level.SEVERE, null, ex);
-            ex.printStackTrace();
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            logger.error("Mail couldn't send: "+toEmail, ex);
         }
     }
 
